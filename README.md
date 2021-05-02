dbccf959d7b5ee123c5411f94e2da5e687e3c5e3
Guilherme Andre <gwandre@gmail.com>
2021-05-02 07:54:26 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # adabas-tcp-test
 ADABAS TCP test program
+
+1. Login to your server;
+2. wget https://github.com/gwandre/adabas-tcp-test/archive/refs/heads/main.zip
+3. sudo apt install unzip
+4. unzip main.zip
+5. cd adabas-tcp-test-main
+6. sudo docker build -t adabas-tcp-test .
+7. sudo docker run --rm -it -p 8080:8080 --name adatcp1 adabas-tcp-test
+8. The container is running (until you close with CTRL+C as you used the --rm parameter)
+9. Press CTRL+P and CTRL+Q to quit. The container will continue running.
+
+THAT'S ALL!
---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const Adabas = require('adabas-tcp').Adabas;
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/adabas', (req, res) => {
+	const adabas = new Adabas('172.20.1.110', 60001);
+	adabas.read({ fnr: 11 }).then( result => {
+		res.send(result);
+		adabas.close().then( () => adabas.disconnect() );
+	});
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
